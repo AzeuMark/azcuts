846c84e6f3f5d0899b7c7600b56ad4f00137be91
Azeu Mark <azeumark@gmail.com>
2026-07-17 23:42:47 +0800

    Ready I guess

--- test.js
+++
@@ -1,9 +0,0 @@
-import { encrypt, decrypt } from './server/models/User.js';
-
-// Test
-const password = 'Admin@123';
-const encrypted = encrypt(password);
-console.log('Original:', password);
-console.log('Encrypted:', encrypted);
-console.log('Decrypted:', decrypt(encrypted));
-console.log('Wrong decrypt test:', decrypt(encrypted) === 'wrong' ? 'Match' : 'No match');
